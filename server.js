45fe09fc46beb186a0790c7369ef90cc0ae68d7b
dev-ab <hellboyon@gmail.com>
2014-09-15 12:35:07 +0300

    latest port

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ io.on('connection', function(socket){
     });
 });
 
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(port, function(){
+    console.log('listening on *:' + port);
 });
